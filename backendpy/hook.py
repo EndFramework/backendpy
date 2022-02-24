559d43059d3ce2994a52e9048dcb75815814d667
hamdollahi <jalil.hamdollahi@gmail.com>
2022-02-24 19:18:43 +0330

    Update type annotations

--- backendpy/hook.py
+++ backendpy/hook.py
@@ -1,50 +1,60 @@
+from __future__ import annotations
+
+from collections.abc import Mapping, Iterable
 from inspect import iscoroutinefunction
-from typing import Dict, List, Callable, AnyStr
+from typing import Optional, Any
 
 
 class Hooks:
-    def __init__(self):
-        self._items: Dict[AnyStr, List[Callable]] = dict()
+    def __init__(self) -> None:
+        self._items: dict[str, list[callable]] = dict()
 
     @property
-    def items(self):
+    def items(self) -> dict[str, list[callable]]:
         return self._items
 
-    def register(self, event_name, func):
-        assert iscoroutinefunction(func)
+    def register(self, event_name: str, func: callable) -> None:
+        if not iscoroutinefunction(func):
+            raise TypeError('The "func" parameter must be an asynchronous function.')
         self._register(event_name, func)
 
-    def register_batch(self, items: Dict[AnyStr, List[Callable]]):
+    def register_batch(self, items: Mapping[str, Iterable[callable]]) -> None:
         for event_name, funcs in items.items():
             for func in funcs:
                 self.register(event_name, func)
 
-    def event(self, name):
-        def decorator_register(func):
+    def event(self, name: str) -> callable:
+        """Register an event hook with python decorator.
+
+        :param name: The name of an event
+        """
+        def decorator_register(func: callable) -> None:
             self.register(name, func)
         return decorator_register
 
-    def merge(self, other):
+    def merge(self, other: Hooks) -> None:
+        """Merge items from another Hooks class instance with this instance."""
         if not isinstance(other, self.__class__):
-            raise TypeError(f'Can not merge the "Hooks" and "{type(other)}"')
+            raise TypeError(f"{type(self.__class__)} and {type(other)} cannot be merged.")
         for event_name, funcs in other.items.items():
             for func in funcs:
                 self._register(event_name, func)
 
-    def _register(self, event_name, func):
+    def _register(self, event_name: str, func: callable) -> None:
         if event_name not in self._items:
-            self._items[event_name]: List[Callable] = list()
+            self._items[event_name]: list[callable] = list()
         self._items[event_name].append(func)
 
-    def __getitem__(self, name):
+    def __getitem__(self, name: str) -> list[callable]:
         return self._items[name]
 
-    def __contains__(self, name):
+    def __contains__(self, name: str) -> bool:
         return name in self._items
 
-    def __add__(self, other):
+    def __add__(self, other: Hooks) -> Hooks:
+        """Concatenate items from two instances of the Hooks class and return a new instance."""
         if not isinstance(other, self.__class__):
-            raise TypeError(f'Can not concat the "Hooks" and "{type(other)}"')
+            raise TypeError(f"{type(self.__class__)} and {type(other)} cannot be concatenated.")
         new = self.__class__()
         new.register_batch(self._items)
         new.register_batch(other.items)
@@ -52,10 +62,15 @@ class Hooks:
 
 
 class HookRunner:
-    def __init__(self):
+    def __init__(self) -> None:
         self.hooks = Hooks()
 
-    async def trigger(self, name, args=None):
+    async def trigger(self, name: str, args: Optional[Mapping[str, Any]] = None) -> None:
+        """Trigger all hooks related to the event.
+
+        :param name: The name of an event
+        :param args: A dictionary-like object containing arguments passed to the hook function.
+        """
         if name in self.hooks:
             for func in self.hooks[name]:
                 if args is not None:
