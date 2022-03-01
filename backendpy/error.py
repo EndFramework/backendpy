7711a82f6947e8ec108017960bb894b1229c3a48
hamdollahi <jalil.hamdollahi@gmail.com>
2022-03-01 21:16:04 +0330

    Update type annotations

--- backendpy/error.py
+++ backendpy/error.py
@@ -1,26 +1,42 @@
-from .response import Status, JSON
+from __future__ import annotations
+
+from collections.abc import Mapping, Iterable, AsyncGenerator
+from typing import Optional, Any
+
+from .request import Request
 from .exception import ExceptionResponse
+from .response import Status, JSON
 
 
 class Error(ExceptionResponse, JSON):
-    def __init__(self, code, message_data=None, data=None, headers=None, compress=False):
-        JSON.__init__(self, body=None, status=None, headers=headers, compress=compress)
+    def __init__(
+            self, code: int,
+            message_data: Optional[Mapping[str, Any] |
+                                   Iterable[Any] |
+                                   Any] = None,
+            data: Optional[Any] = None,
+            headers: Optional[Iterable[[bytes, bytes]]] = None,
+            compress: bool = False) -> None:
+        JSON.__init__(self, body=None, headers=headers, compress=compress)
         self.code = code
-        self.data = data
         self.message_data = message_data
+        self.data = data
 
-    async def __call__(self, request):
+    async def __call__(self, request: Request) \
+            -> tuple[bytes | AsyncGenerator[bytes],
+                     int,
+                     list[[bytes, bytes]],
+                     bool]:
         error = request.app.errors[self.code]
-
         self.status = error.status
         self.body = {
             'status': 'error',
             'code': self.code}
 
         if self.message_data:
-            if type(self.message_data) is dict:
+            if isinstance(self.message_data, Mapping):
                 self.body['message'] = error.message.format(**self.message_data)
-            elif type(self.message_data) is tuple:
+            elif isinstance(self.message_data, Iterable):
                 self.body['message'] = error.message.format(*self.message_data)
             else:
                 self.body['message'] = error.message.format(self.message_data)
@@ -29,54 +45,58 @@ class Error(ExceptionResponse, JSON):
 
         if self.data is not None:
             self.body['data'] = self.data
+
         return await JSON.__call__(self, request)
 
 
 class ErrorCode:
-    def __init__(self, code, message, status):
-        self._code = code
-        self._message = message
-        self._status = status
+    def __init__(self, code: int, message: str, status: Status) -> None:
+        self._code: int = code
+        self._message: str = message
+        self._status: Status = status
 
     @property
-    def code(self):
+    def code(self) -> int:
         return self._code
 
     @property
-    def message(self):
+    def message(self) -> str:
         return self._message
 
     @property
-    def status(self):
+    def status(self) -> Status:
         return self._status
 
 
 class ErrorList:
-    def __init__(self, *codes):
-        self._items = dict()
+    def __init__(self, *codes: ErrorCode) -> None:
+        self._items: dict[int, ErrorCode] = dict()
         self.extend(*codes)
 
     @property
-    def items(self):
+    def items(self) -> dict[int, ErrorCode]:
+        """Get items"""
         return self._items
 
-    def extend(self, *codes):
+    def extend(self, *codes: ErrorCode) -> None:
+        """Extend items"""
         for i in codes:
             if not isinstance(i, ErrorCode):
-                raise Exception(f'invalid error code defined')
+                raise Exception('Invalid error code type')
             if i.code in self._items:
-                raise Exception(f'duplicate error code "{i.code}" defined')
+                raise Exception(f'Duplicate error code "{i.code}"')
             self._items[i.code] = i
 
-    def merge(self, other):
+    def merge(self, other: ErrorList) -> None:
+        """Merge items from another ErrorList class instance with this instance."""
         if not isinstance(other, self.__class__):
-            raise TypeError(f'can not merge the "ErrorList" and "{type(other)}"')
+            raise TypeError(f"{type(self.__class__)} and {type(other)} cannot be merged.")
         for code, i in other.items.items():
             if code in self._items:
-                raise Exception(f'duplicate error code "{code}" defined')
+                raise Exception(f'Duplicate error code "{code}"')
             self._items[code] = i
 
-    def __getitem__(self, item):
+    def __getitem__(self, item) -> ErrorCode:
         return self._items[item]
 
 
--- backendpy/exception.py
+++ backendpy/exception.py
@@ -1,51 +1,93 @@
-from .utils.json import to_json
+from __future__ import annotations
+
+from collections.abc import Iterable, Mapping, AsyncGenerator
+from typing import Optional, Any
+
+from .request import Request
 from .response import Status, Response
+from .utils.json import to_json
 
 
 class ExceptionResponse(BaseException, Response):
-    def __init__(self, body, status=Status.OK, headers=None, content_type=b'text/plain', compress=False):
+    def __init__(
+            self,
+            body: Any,
+            status: Status = Status.OK,
+            headers: Optional[Iterable[[bytes, bytes]]] = None,
+            content_type: bytes = b'text/plain',
+            compress: bool = False) -> None:
         BaseException.__init__(self)
-        Response.__init__(self, body=body, status=status, headers=headers, content_type=content_type, compress=compress)
+        Response.__init__(
+            self,
+            body=body,
+            status=status,
+            headers=headers,
+            content_type=content_type,
+            compress=compress)
 
-    async def __call__(self, *args, **kwargs):
-        return await super().__call__(*args, **kwargs)
+    async def __call__(self, request: Request) \
+            -> tuple[bytes | AsyncGenerator[bytes],
+                     int,
+                     list[[bytes, bytes]],
+                     bool]:
+        return await super().__call__(request)
 
 
 class BadRequest(ExceptionResponse):
-    def __init__(self, body=b'Bad Request', content_type=None):
-        super().__init__(body=body if type(body) is not dict else to_json(body),
-                         status=Status.BAD_REQUEST,
-                         content_type=content_type if content_type else
-                         (b'text/plain' if type(body) is not dict else b'application/json'))
+    def __init__(
+            self,
+            body: Any = Status.BAD_REQUEST.description,
+            content_type: Optional[bytes] = None) -> None:
+        super().__init__(
+            body=body if not isinstance(body, Mapping) else to_json(body),
+            status=Status.BAD_REQUEST,
+            content_type=content_type if content_type else
+            (b'text/plain' if not isinstance(body, Mapping) else b'application/json'))
 
 
 class Unauthorized(ExceptionResponse):
-    def __init__(self, body=b'Unauthorized', content_type=None):
-        super().__init__(body=body if type(body) is not dict else to_json(body),
-                         status=Status.UNAUTHORIZED,
-                         content_type=content_type if content_type else
-                         (b'text/plain' if type(body) is not dict else b'application/json'))
+    def __init__(
+            self,
+            body: Any = Status.UNAUTHORIZED.description,
+            content_type: Optional[bytes] = None) -> None:
+        super().__init__(
+            body=body if not isinstance(body, Mapping) else to_json(body),
+            status=Status.UNAUTHORIZED,
+            content_type=content_type if content_type else
+            (b'text/plain' if not isinstance(body, Mapping) else b'application/json'))
 
 
 class Forbidden(ExceptionResponse):
-    def __init__(self, body=b'Forbidden', content_type=None):
-        super().__init__(body=body if type(body) is not dict else to_json(body),
-                         status=Status.FORBIDDEN,
-                         content_type=content_type if content_type else
-                         (b'text/plain' if type(body) is not dict else b'application/json'))
+    def __init__(
+            self,
+            body: Any = Status.FORBIDDEN.description,
+            content_type: Optional[bytes] = None) -> None:
+        super().__init__(
+            body=body if not isinstance(body, Mapping) else to_json(body),
+            status=Status.FORBIDDEN,
+            content_type=content_type if content_type else
+            (b'text/plain' if not isinstance(body, Mapping) else b'application/json'))
 
 
 class NotFound(ExceptionResponse):
-    def __init__(self, body=b'Not Found', content_type=None):
-        super().__init__(body=body if type(body) is not dict else to_json(body),
-                         status=Status.NOT_FOUND,
-                         content_type=content_type if content_type else
-                         (b'text/plain' if type(body) is not dict else b'application/json'))
+    def __init__(
+            self,
+            body: Any = Status.NOT_FOUND.description,
+            content_type: Optional[bytes] = None) -> None:
+        super().__init__(
+            body=body if not isinstance(body, Mapping) else to_json(body),
+            status=Status.NOT_FOUND,
+            content_type=content_type if content_type else
+            (b'text/plain' if not isinstance(body, Mapping) else b'application/json'))
 
 
 class ServerError(ExceptionResponse):
-    def __init__(self, body=b'Internal Server Error', content_type=None):
-        super().__init__(body=body if type(body) is not dict else to_json(body),
-                         status=Status.INTERNAL_SERVER_ERROR,
-                         content_type=content_type if content_type else
-                         (b'text/plain' if type(body) is not dict else b'application/json'))
+    def __init__(
+            self,
+            body: Any = Status.INTERNAL_SERVER_ERROR.description,
+            content_type: Optional[bytes] = None) -> None:
+        super().__init__(
+            body=body if not isinstance(body, Mapping) else to_json(body),
+            status=Status.INTERNAL_SERVER_ERROR,
+            content_type=content_type if content_type else
+            (b'text/plain' if not isinstance(body, Mapping) else b'application/json'))
