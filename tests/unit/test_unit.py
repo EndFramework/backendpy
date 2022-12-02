2dbd18aac6a2eaf6a64239df01ccef4f42ad317b
hamdollahi <jalil.hamdollahi@gmail.com>
2022-12-03 01:46:36 +0330

    Clear tests

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -1,11 +1,3 @@
-print(int(float(1)))
-print(int(float('1')))
-print(int(float(1.0)))
-print(int(float(1.5)))
-print(int(float('1.0')))
-print(int(float('1.5')))
-
-
 from backendpy.unittest import AsyncTestCase
 
 
